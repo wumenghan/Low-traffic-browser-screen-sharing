e91e4a1e31d4e282798fb6c7d89b4a8c4ed09442
gaopinghuang0 <gaopinghuang@gmail.com>
2018-04-21 17:23:13 -0400

    update repo structure

--- main_project/README.md
+++ main_project/README.md
@@ -1,13 +1,28 @@
-## Main project
+# Main project
 
+## Getting Started
+```sh
+$ python -m SimpleHTTPServer
+# Then visit the localhost:8000/requester.html
+```
 
-## Event simulate examples
+## Resources
 * Simulate mouse events <https://github.com/kangax/protolicious/blob/master/event.simulate.js> and <https://stackoverflow.com/questions/6157929/how-to-simulate-a-mouse-click-using-javascript>
 * Get Xpath, `getPathTo` - <https://stackoverflow.com/questions/2631820/how-do-i-ensure-saved-click-coordinates-can-be-reloaed-to-the-same-place-even-i/2631931#2631931> and <https://stackoverflow.com/questions/36452390/get-xpath-of-a-dom-element-with-jquery>
 * Select by Xpath, `jquery.xpath.js` - <https://stackoverflow.com/questions/6453269/jquery-select-element-by-xpath> and <https://github.com/ilinsky/jquery-xpath>
+* Web video player, [video.js](https://videojs.com/) and [videojs-panorama](https://github.com/yanwsh/videojs-panorama) which supports 360 video
+* [Free 360 video download](https://www.mettle.com/360vr-master-series-free-360-downloads-page/)
+
+
+## Fatal Issues with Iframe:
+[Hard to Detect Click into Iframe using JavaScript](https://stackoverflow.com/questions/2381336/detect-click-into-iframe-using-javascript)
+* We can at most know that we click on Iframe element, but cannot know the clicked elements, let alone simulate mouse events into Iframe
+* Therefore, it's impossible to embed Youtube.
+* Solution: use [video.js](https://videojs.com/) to load video in the same page.
+
 
 
 ## TODO:
-* Mouse cursor move
+* Simulate mouse cursor
 * Input and Keystroke
-* Drag
+* Drag and drop
---
+++ main_project/js/replay.js
@@ -0,0 +1,116 @@
+
+
+(function($) {
+
+
+// TODO: determine mode
+var mode = 'replay' || 'realtime';
+
+var specialHandlers = {
+  'resize': resizeHandler,
+}
+
+$(document).ready(function() {
+  recordMouseClick();
+  if (mode == 'realtime') {
+    // TODO
+    console.log("In realtime mode..");
+  } else {
+    console.log("In replay mode..");
+    startReplay();
+  }
+});
+
+function startReplay() {
+  var commands = loadCommands();
+  playCommand(commands);
+}
+
+function loadCommands() {
+  // TODO: load from json
+  // TODO: load from server API
+  var commands = [
+    // {
+    //   delay: 1000,
+    //   eventName: 'resize',
+    //   args: {width: 1000, height: 800}
+    // },
+    // {
+    //   delay: 2000,
+    //   eventName: 'resize',
+    //   args: {width: 800, height: 600}
+    // },
+    {
+      delay: 2000,
+      eventName: 'click',
+      args: {pointerX: 65 , pointerY: 58},
+      xpath: 'id("first-btn")'
+    },
+    // {
+    //   delay: 2000,
+    //   eventName: 'click',
+    //   args: {pointerX: 206 , pointerY: 42},
+    //   xpath: 'BODY/P[1]/BUTTON[1]'
+    // },
+    // {
+    //   delay: 2000,
+    //   eventName: 'click',
+    //   args: {pointerX: 117 , pointerY: 157},
+    //   xpath: 'id("second-p")/A[1]'
+    // },
+    // {
+    //   delay: 2000,
+    //   eventName: 'click',
+    //   args: {pointerX: 492 , pointerY: 253},
+    //   xpath: 'id("second-p")/BUTTON[2]'
+    // },
+  ];
+  return commands;
+}
+
+function playCommand(commandQueue) {
+  let cmd;
+  while (true) {
+    if (commandQueue.length === 0) return;
+    
+    cmd = commandQueue.shift();  // get first command from the Queue
+    if (cmd && typeof cmd.delay !== 'undefined' && typeof cmd.eventName !== 'undefined')
+      break;
+  }
+  let delay = cmd.delay;
+  let eventName = cmd.eventName;
+  let args = cmd.args;
+  let xpath = cmd.xpath;
+
+  setTimeout(function() {
+    console.log(delay, eventName, args);
+    if (eventName in specialHandlers) {
+      specialHandlers[eventName](document.documentElement, args);
+    }
+    else {
+      // console.log(xpath, getElementByXpath(xpath))
+      simulate(getElementByXpath(xpath), eventName, args);
+    }
+
+    // call the rest commandQueue
+    playCommand(commandQueue);
+  }, delay);
+}
+
+function resizeHandler(element, args) {
+  window.resizeTo(args.width, args.height);
+}
+
+function recordMouseClick() {
+  $("body").on('click', function(evt) {
+    console.log('clicked at', evt.clientX, evt.clientY, 'xpath:', getPathTo(evt.target));
+
+    // alert('clicked at'+ evt.clientX+ evt.clientY);
+  })
+  $("#first-btn").on('click', function() {
+    // alert('clicked at me')
+  })
+}
+
+
+})(jQuery);
